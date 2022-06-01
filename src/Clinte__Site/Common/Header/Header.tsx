6d6ca47b3109f9706d537b336d10ec41b4493f47
amitbd599 <amitbd599@gmail.com>
2022-06-02 02:55:25 +0600

    first commit

---
+++ src/Clinte__Site/Common/Header/Header.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Container, Row, Col } from "react-bootstrap";
+const Header = () => {
+  return (
+    <section className="header__section">
+      {/* Top header section start */}
+      <div className="top__headerSection">
+        <Container>
+          <Row>
+            <Col>
+                
+            </Col>
+          </Row>
+          hello
+        </Container>
+      </div>
+      {/* Top header section End */}
+    </section>
+  )
+}
+
+export default Header
---
+++ src/Clinte__Site/Page/Home.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "../Common/Header/Header";
+const Home = () => {
+  return (
+    <>
+      {/* Header Section start */}
+      <Header />
+      {/* Header Section End */}
+    </>
+  );
+};
+
+export default Home;
