496c490a254ccfca4e7d665e055820bc0d0e4325
amitbd599 <amitbd599@gmail.com>
2022-06-13 02:42:51 +0600

    add about page

--- src/Clinte__Site/Page/About.tsx
+++ src/Clinte__Site/Page/About.tsx
@@ -2,15 +2,23 @@ import { title } from "process";
 import React, { useState } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import { BsFillArrowRightCircleFill } from "react-icons/bs";
+import {
+  FaFacebookSquare,
+  FaInstagramSquare,
+  FaLinkedin,
+  FaTwitterSquare,
+} from "react-icons/fa";
 import { Link } from "react-router-dom";
 import Footer from "../Common/Footer/Footer";
 import Header from "../Common/Header/Header";
 import Intro__Section from "../Common/IntroSection/Intro__Section";
 import Meta_Data from "../Common/Meta_Data";
 import Product_Data from "../Script/Product__Data";
+import Team__Data from "../Script/Team__Data";
 
 const About: React.FC = () => {
   const { productImgFile } = Product_Data();
+
   return (
     <>
       {/* Helmet Intro Start */}
@@ -36,149 +44,192 @@ const About: React.FC = () => {
         <div className="wrapper">
           <div className="wrapper__body">
             <Container>
-              <Row className="part__one">
-                <Col lg={5} className="left__side">
-                  <div className="inner__body">
-                    <img
-                      className="img-fluid"
-                      src="/assets/images/about__section/about__model-1.png"
-                      alt=""
-                    />
-                  </div>
-                </Col>
-                <Col lg={7} className="right__side">
-                  <div className="inner__body">
-                    <h2>
-                      Elehaus Story, We Craft Awesome With Great Experiences.
-                    </h2>
-                    <p>
-                      Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-                      sed do eiusmo tempor incididunt ut labore et dolore magna
-                      aliqua. Ut enim ad minimo veniam, quis nostrud
-                      exercitation ullamco laboris nisi ut aliquip ex eatrl
-                      commodo consequat. Duis aute irure dolor in reprehenderit
-                      in voluptat velit esse cillum dolore eu fugiat nulla
-                      pariatur.
-                    </p>
+              <div className="part__one">
+                <Row>
+                  <Col lg={5} className="left__side">
+                    <div className="inner__body">
+                      <img
+                        className="img-fluid"
+                        src="/assets/images/about__section/about__model-1.png"
+                        alt=""
+                      />
+                    </div>
+                  </Col>
+                  <Col lg={7} className="right__side">
+                    <div className="inner__body">
+                      <h2>
+                        Elehaus Story, We Craft Awesome With Great Experiences.
+                      </h2>
+                      <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing
+                        elit, sed do eiusmo tempor incididunt ut labore et
+                        dolore magna aliqua. Ut enim ad minimo veniam, quis
+                        nostrud exercitation ullamco laboris nisi ut aliquip ex
+                        eatrl commodo consequat. Duis aute irure dolor in
+                        reprehenderit in voluptat velit esse cillum dolore eu
+                        fugiat nulla pariatur.
+                      </p>
 
-                    <div>
-                      <Link
-                        to={"/contact"}
-                        className="button__mid_solid_color_black"
-                      >
-                        Contact Now
-                      </Link>
+                      <div>
+                        <Link
+                          to={"/contact"}
+                          className="button__mid_solid_color_black"
+                        >
+                          Contact Now
+                        </Link>
+                      </div>
+                    </div>
+                  </Col>
+                </Row>
+              </div>
+              <div className="part__two">
+                <Row>
+                  <Col lg={7} className="left__side">
+                    <div className="inner__body">
+                      <h2>
+                        Stand Male Casual Coats Street Chamarras Para Hombre.
+                      </h2>
+
+                      <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Reiciendis tempore ab itaque quam consequuntur illo ipsa
+                        debitis rem quae eaque eligendi doloremque aspernatur
+                        voluptates recusandae labore quia aperiam, asperiores
+                        excepturi laudantium nostrum cum assumenda commodi
+                        voluptatibus? Voluptatem illo odio et!
+                      </p>
+                      <ul>
+                        <li>
+                          {" "}
+                          <BsFillArrowRightCircleFill className="icon" /> Please
+                          note 1cm=0.39inch; 1inch=2.54cm.
+                        </li>
+                        <li>
+                          {" "}
+                          <BsFillArrowRightCircleFill className="icon" />{" "}
+                          Measurement (Unit: CM) Original design-100% Real
+                          Photos.
+                        </li>
+                        <li>
+                          {" "}
+                          <BsFillArrowRightCircleFill className="icon" />
+                          Please allow 1-2cm (0.4-0.8") differences due to
+                          manual measurement.
+                        </li>
+                        <li>
+                          {" "}
+                          <BsFillArrowRightCircleFill className="icon" />
+                          Item color displayed in photos may be showing the
+                          correct color.
+                        </li>
+                        <li>
+                          {" "}
+                          <BsFillArrowRightCircleFill className="icon" />
+                          Please contact us if you have any questions about our
+                          available sizes.
+                        </li>
+                        <li>
+                          {" "}
+                          <BsFillArrowRightCircleFill className="icon" />
+                          Awesome allow 1-2cm (0.4-0.8") differences due to auto
+                          measurement.
+                        </li>
+                      </ul>
+
+                      <div>
+                        <Link
+                          to={"/contact"}
+                          className="button__mid_solid_color_black"
+                        >
+                          Get Discount
+                        </Link>
+                      </div>
+                    </div>
+                  </Col>
+                  <Col lg={5} className="right__side">
+                    <div className="inner__body">
+                      <img
+                        className="img-fluid"
+                        src="/assets/images/about__section/about__model-3.png"
+                        alt=""
+                      />
+                    </div>
+                  </Col>
+                </Row>
+              </div>
+              <div className="part__three">
+                <Row>
+                  <Col>
+                    <div className="inner__body">
+                      <h2>Our Team</h2>
+
+                      <div className="team__items">
+                        <Row>
+                          {Team__Data.map((value, index) => (
+                            <Col lg={3} md={4} sm={6}>
+                              <div className="img__file">
+                                <img
+                                  className="img-fluid"
+                                  src={value.img}
+                                  alt=""
+                                />
+                                <div className="text__file">
+                                  <div className="inner__text">
+                                    <h3>{value.name}</h3>
+                                    <p>{value.position}</p>
+                                    <div className="icon__file">
+                                      <a href={value.fb_link}>
+                                        <FaFacebookSquare className="icon" />
+                                      </a>
+                                      <a href={value.tw_link}>
+                                        <FaTwitterSquare className="icon" />
+                                      </a>
+                                      <a href={value.li_link}>
+                                        <FaLinkedin className="icon" />
+                                      </a>
+                                      <a href={value.in_link}>
+                                        <FaInstagramSquare className="icon" />
+                                      </a>
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
+                            </Col>
+                          ))}
+                        </Row>
+                      </div>
+                    </div>
+                  </Col>
+                </Row>
+              </div>
+              <div className="part__four">
+                <Row>
+                  <div className="inner__body">
+                    <div className="header">
+                      <h2>
+                        Smart Fashion With <br />{" "}
+                        <span className="theme-text">Smart Devices</span>
+                      </h2>
+                      <div className="img__section">
+                        <Row>
+                          <Col lg={12} md={6} sm={12}>
+                            <div className="leftImg__section">
+                              {productImgFile.map((value, index) => (
+                                <img
+                                  key={index}
+                                  className="img-fluid"
+                                  src={value.src}
+                                  alt=""
+                                />
+                              ))}
+                            </div>
+                          </Col>
+                        </Row>
+                      </div>
                     </div>
                   </div>
-                </Col>
-              </Row>
-              <Row className="part__two">
-                <Col lg={7} className="left__side">
-                  <div className="inner__body">
-                    <h2>
-                      Stand Male Casual Coats Street Chamarras Para Hombre.
-                    </h2>
-
-                    <p>
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Reiciendis tempore ab itaque quam consequuntur illo ipsa
-                      debitis rem quae eaque eligendi doloremque aspernatur
-                      voluptates recusandae labore quia aperiam, asperiores
-                      excepturi laudantium nostrum cum assumenda commodi
-                      voluptatibus? Voluptatem illo odio et!
-                    </p>
-                    <ul>
-                      <li>
-                        {" "}
-                        <BsFillArrowRightCircleFill className="icon" /> Please
-                        note 1cm=0.39inch; 1inch=2.54cm.
-                      </li>
-                      <li>
-                        {" "}
-                        <BsFillArrowRightCircleFill className="icon" />{" "}
-                        Measurement (Unit: CM) Original design-100% Real Photos.
-                      </li>
-                      <li>
-                        {" "}
-                        <BsFillArrowRightCircleFill className="icon" />
-                        Please allow 1-2cm (0.4-0.8") differences due to manual
-                        measurement.
-                      </li>
-                      <li>
-                        {" "}
-                        <BsFillArrowRightCircleFill className="icon" />
-                        Item color displayed in photos may be showing the
-                        correct color.
-                      </li>
-                      <li>
-                        {" "}
-                        <BsFillArrowRightCircleFill className="icon" />
-                        Please contact us if you have any questions about our
-                        available sizes.
-                      </li>
-                      <li>
-                        {" "}
-                        <BsFillArrowRightCircleFill className="icon" />
-                        Awesome allow 1-2cm (0.4-0.8") differences due to auto
-                        measurement.
-                      </li>
-                    </ul>
-
-                    <div>
-                      <Link
-                        to={"/contact"}
-                        className="button__mid_solid_color_black"
-                      >
-                        Get Discount
-                      </Link>
-                    </div>
-                  </div>
-                </Col>
-                <Col lg={5} className="right__side">
-                  <div className="inner__body">
-                    <img
-                      className="img-fluid"
-                      src="/assets/images/about__section/about__model-3.png"
-                      alt=""
-                    />
-                  </div>
-                </Col>
-              </Row>
-              <Row className="part__three">
-                <div className="inner__body">
-                  <div className="header">
-                    <h2>
-                      Smart Fashion With <br />{" "}
-                      <span className="theme-text">Smart Devices</span>
-                    </h2>
-                    <div className="img__section">
-                      <Row>
-                        <Col lg={12} md={6} sm={12}>
-                          <div className="leftImg__section">
-                            {productImgFile.map((value, index) => (
-                              <img
-                                key={index}
-                                className="img-fluid"
-                                src={value.src}
-                                alt=""
-                              />
-                            ))}
-                          </div>
-                        </Col>
-                        {/* <Col lg={6} md={6} sm={12}>
-                          <div className="rightImg__section">
-                            <img
-                              className="img-fluid"
-                              src="/assets/images/about__section/about__model-6.png"
-                              alt=""
-                            />
-                          </div>
-                        </Col> */}
-                      </Row>
-                    </div>
-                  </div>
-                </div>
-              </Row>
+                </Row>
+              </div>
             </Container>
           </div>
         </div>
---
+++ src/Clinte__Site/Script/Team__Data.ts
@@ -0,0 +1,50 @@
+type TeamDataType = {
+  name?: string;
+  position?: string;
+  img?: string;
+  fb_link?: string;
+  tw_link?: string;
+  li_link?: string;
+  in_link?: string;
+};
+
+let Team__Data: TeamDataType[] = [
+  {
+    name: "Daniel B. Crespo",
+    position: "Conference planner",
+    img: "/assets/images/team/team-1.png",
+    fb_link: "/",
+    tw_link: "/",
+    li_link: "/",
+    in_link: "/",
+  },
+  {
+    name: "Dannette J. Puckett",
+    position: "Tax collector",
+    img: "/assets/images/team/team-2.png",
+    fb_link: "/",
+    tw_link: "/",
+    li_link: "/",
+    in_link: "/",
+  },
+  {
+    name: "Dominic J. Botkin",
+    position: "Embalmer",
+    img: "/assets/images/team/team-3.png",
+    fb_link: "/",
+    tw_link: "/",
+    li_link: "/",
+    in_link: "/",
+  },
+  {
+    name: "Dorthy J. Pond",
+    position: "Septic tank servicer",
+    img: "/assets/images/team/team-4.png",
+    fb_link: "/",
+    tw_link: "/",
+    li_link: "/",
+    in_link: "/",
+  },
+];
+
+export default Team__Data;
